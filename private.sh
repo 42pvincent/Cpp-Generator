af47828176feac62bacf750acbd7ef8d2065eb1a
pvincent <paul.vincent.ecole@gmail.com>
2015-01-08 04:31:12 +0100

    add private.sh

---
+++ private.sh
@@ -0,0 +1,37 @@
+class=$1
+type=$2
+name=$3
+
+echo "" >> $class.class.cpp
+echo $2 >> $class.class.cpp
+echo "$class::get$name(void) const
+{
+	return (this->_$name);
+}" >> $class.class.cpp
+
+echo "" >> $class.class.cpp
+echo "void" >> $class.class.cpp
+echo "$class::set$name($type const $name)
+{
+	this->_$name = $name;
+	return ;
+}" >> $class.class.cpp
+
+sed -i '.tmp' $'/private:/ i\
+\\\t\t'"$2 "'get'"$3"'(void)'" const;
+" $1.class.hpp
+sed -i '.tmp' $'/private:/ i\
+\\\t\t'"void "'set'"$3($2 const $3);\\
+\\
+"  $1.class.hpp
+sed -i '.tmp' $'/private:/ a\
+\\\t\t'"$2"$'\\\t'"_$3;\\
+\\
+"  $1.class.hpp
+
+
+
+
+rm -rf $1.class.hpp.tmp
+
+
