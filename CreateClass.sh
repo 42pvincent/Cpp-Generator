29ad38a17277f17f60b6fb9c661cead05bab742b
Florian Gundlack <flogundlack@gmail.com>
2015-01-13 04:57:36 +0100

    added secret option of doom

--- CreateClass.sh
+++ CreateClass.sh
@@ -1,4 +1,17 @@
-if [ $1 ]; then
+if [ $1 == "Quake3" ]; then
+git clone http://github.com/ioquake/ioq3.git quake3-tmp
+sh quake3-tmp/make-macosx.sh x86_64
+mkdir ioquake3
+mv quake3-tmp/build/release-darwin-x86_64/ ./ioquake3/
+curl -o quake3-tmp/baseq3.zip "http://wingu.trektawimbi.fr/public.php?service=files&t=a234fc4f4f4d4b7882c0ef3316d56a6e&download"
+unzip quake3-tmp/baseq3.zip -d quake3-tmp/
+mv ioquake3/release-darwin-x86_64/* ioquake3/
+rm -rf ioquake3/baseq3
+mv quake3-tmp/baseq3 ./ioquake3/
+rm -rf quake3-tmp
+fi
+
+if [ $1 ] && [ $1 != "Quake3" ] ; then
 class=$1
 CLASS=`echo "$class" | tr '[:lower:]' '[:upper:]'`
 CLASS+="_CLASS_HPP"
